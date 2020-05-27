84f4c39ef093718f7d81f6e026aeb3574a8588de
HARAQUEL <haustriaraquel@gmail.com>
2020-05-27 14:58:37 -0500

    Update and rename Script_1.sh to Script1.sh

--- bin/Script1.sh
+++ bin/Script1.sh
@@ -1,10 +1,9 @@
 #!/bin/bash
-## Raquel Hernández Austria
+## Raquel Hernández Austria Mayo 2020
 ## Script 1: Quality control with FastQC
 
 # Preliminaries: Preparing directories
 # Make a directory named Proyecto_final on raquelha directory in UChile`s cluster.
-
 mkdir Proyecto_final
 
 # Download data in Proyecto_final
@@ -19,10 +18,8 @@ mkdir bin
 mkdir fastq_raw
 
 # From bin, evaluate the quality of lane1`s reads with Fastqc
-
 /opt/fastQC_v0.11.2/FastQC/fastqc ../raw/lane1/*.fq.gz -o ../fastq_raw
 
 # .html and .zip files are generated; download the files to your computer to assess the quality of the reads
-
 scp bioinfo1@genoma.med.uchile.cl:/home/bioinfo1/raquelha/Proyecto_final/rochette2017_gac_or/top/fastq_raw/lane1* .
 
