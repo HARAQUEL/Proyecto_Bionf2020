72ca67e89bb7d93bc9d81aad09858fac9ad951cd
HARAQUEL <haustriaraquel@gmail.com>
2020-05-26 21:01:12 -0500

    Update Script_1.sh

--- bin/Script_1.sh
+++ bin/Script_1.sh
@@ -1,7 +1,18 @@
-**Script 1: Quality control with FastQC**
+## Raquel Hernández Austria
+## Script 1: Quality control with FastQC
 
-#Evaluate the quality of the reads with Fastqc
-#On  “Proyecto_final” directory make up “bin” and “fastq_raw” directories
+# Preliminaries: Preparing directories
+# Make a directory with subdirectories on raquelha directory in UChile`s cluster.
 
-`mkdir bin`
-`mkdir fastq_raw`
+mkdir - p Proyecto_final/{bin,data,fastq_raw}
+
+# From the bin directory, copy stacks_tut_small/raw/lane1/lane1_NoIndex_L001_R1_001.fq.gz to “data” directory from CONABIO`s cluster
+
+scp -r -P 45789 cirio@200.12.166.164:/persistence/cirio/raquelha/stacks_tut_small/raw/lane1/lane1_NoIndex_L001_R1_001.fq.gz data
+
+# Evaluate the quality of the reads with Fastqc
+
+fastqc ../data/lane1_NoIndex_L001_R1_001.fq.gz -o fastq_raw
+
+fastqc ../data/lane1_NoIndex_L001_R1_002.fq.gz -o fastq_raw
+
