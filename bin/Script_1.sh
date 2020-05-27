7b5c150928277afb04f0306a5b1437248eb4e9ad
HARAQUEL <haustriaraquel@gmail.com>
2020-05-26 20:37:48 -0500

    Update and rename Script_1 to Script_1.sh

---
+++ bin/Script_1.sh
@@ -0,0 +1,7 @@
+**Script 1: Quality control with FastQC**
+
+#Evaluate the quality of the reads with Fastqc
+#On  “Proyecto_final” directory make up “bin” and “fastq_raw” directories
+
+`mkdir bin`
+`mkdir fastq_raw`
