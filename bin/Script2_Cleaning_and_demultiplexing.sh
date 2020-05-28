1ee9c74f3b26d8292782dff848f2c24807797b77
HARAQUEL <haustriaraquel@gmail.com>
2020-05-28 18:36:47 -0500

    Create Script2_Cleaning_and_demultiplexing.sh
    Add script for cleaning and demultiplexing my data

---
+++ bin/Script2_Cleaning_and_demultiplexing.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+## Raquel Hernández Austria Mayo 2020
+## Script 2: Cleaning and demultiplexing with Stacks`s functions
+
+# From CONABIO`s cluster, go to the raquelha/stacks_tut_small/demo_scripts/ directory
+# Create variables with relative paths
+
+raw_dir=../raw/lane1/
+barcodes_file=../info/barcodes.lane1.tsv
+out_dir=../cleaned/
+
+# Run process_radtags to demultiplex according to your barcode type
+process_radtags -p $raw_dir -b $barcodes_file -o $out_dir -e sbfI --inline_null -c -q -r
+
+# Download the .log file to you computer
+# For this, make a directory and subdirectories in your computer
+mkdir -p Proyecto_final/{bin,data}
+
+# Then, download the .log file to bin directory
+# The dot at the end means that the file will be saved in the folder where you are (it should be in bin)
+scp -P 45789 cirio@200.12.166.164:/persistence/cirio/raquelha/stacks_tut_small/cleaned/process_radtags.lane1.log .
+
+
