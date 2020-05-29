bae71ff12760822c8c0cdab916069cd86dcd565c
HARAQUEL <haustriaraquel@gmail.com>
2020-05-28 21:10:48 -0500

    Remove gray background and hide the gridlines
    Add command line for remove gray background and hide all the gridlines of the plot

--- bin/Script4_Plot_number_of_reads.R
+++ bin/Script4_Plot_number_of_reads.R
@@ -18,8 +18,11 @@ data <- readr::read_tsv('../data/n_reads_per_sample.tsv')
 data
 
 # Make a bar plot of number of reads by sample
-barplot <- ggplot(data=data, aes(x=Filename, y=Total, fill=Filename)) + #Graphics number of reads (Total) by Filename
-  geom_bar(colour="black", stat="identity") #"black"> Add a black outline ; "identity"> It leaves the data as is
+barplot <- ggplot(data=data, aes(x=Filename, y=Total, fill=Filename)) + # Graphics number of reads (Total) by Filename
+  geom_bar(colour="black", stat="identity") + # "black"> Add a black outline ; "identity"> It leaves the data as is
+  theme_bw() + # Remove gray background
+  theme(panel.grid.minor=element_blank(), # Hide all the gridlines
+        panel.grid.major=element_blank())
 rot_plot <- barplot + theme(axis.text.x = element_text(angle = 45, hjust = 1, size=9)) # Rotate sample names in x-axis
 rot_plot + guides(fill=FALSE) + # Delete legend
   theme(axis.title.x = element_blank()) +   # Remove x-axis label
