d4d3f6a92ab8b06fe6c4d9b8fcd8da7cd471d1c4
HARAQUEL <haustriaraquel@gmail.com>
2020-05-29 20:39:41 -0500

    Correct grammar

--- bin/Script3__Extract_number_of_reads.sh
+++ bin/Script3__Extract_number_of_reads.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 ## Raquel Hernández Austria Mayo 2020
-## Extract the number of reads from the log of process_radtags
+## Extract the number of reads from the log file of process_radtags
 
-# In your computer, go to the Proyecto_final directory
+# On your computer, go to the Proyecto_final directory
 # From bin, retrieve the part of the log between 'Barcode...' and the next empty line
 sed -n '/^Barcode\tFilename\t/,/^$/ p' ../data/process_radtags.lane1.log >> n_reads_per_sample.tsv | mv n_reads_per_sample.tsv ../data
 
-#Yor file are ready for make plots in R!
+#Your file are ready to make plots in R!
